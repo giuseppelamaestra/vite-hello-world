f3a5e6cdf4b4119cd52d5fd4e4cbf0ee99ba6100
giuseppelamaestra <giuseppe.la.maestra@gmail.com>
2023-06-14 17:55:53 +0200

    added text in my component

--- vite-hello-world/src/components/First-Vue-App.vue
+++ vite-hello-world/src/components/First-Vue-App.vue
@@ -1,5 +1,14 @@
 <script></script>
 <template>
-  
+   <h1>
+    LA MIA PRIMA APP CON VITE 
+   </h1>
 </template>
-<style></style>
+<style scoped>
+   h1{
+     margin: 0 auto;
+     text-align: center;
+     font-size: 3rem;
+     color: blueviolet;
+   }
+</style>
